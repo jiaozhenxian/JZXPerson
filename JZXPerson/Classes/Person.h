369def8de29ef0189f8d6c25a16691db7b93615c
焦振先 <565594904@qq.com>
2018-12-11 17:26:55 +0800

    添加spec

---
+++ JZXPerson/Classes/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  JZXPerson
+//
+//  Created by 焦振先 on 2018/12/11.
+//  Copyright © 2018 JZX. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JZXPerson/Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  JZXPerson
+//
+//  Created by 焦振先 on 2018/12/11.
+//  Copyright © 2018 JZX. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
